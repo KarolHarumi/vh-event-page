5dc8f0720f2174948c196fc65e441c0d31047e76
Karol Harumi <karol.harumi@hotmail.com>
2020-08-30 12:22:52 -0600

    Apply for an event method

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ function closeModal() {
     });
 }
 
-function openModal() {
-    document.getElementById('modal').classList.remove('hide');
-}
+// function openModal() {
+//     document.getElementById('modal').classList.remove('hide');
+// }
 
 function moreDetails() {
     let buttons = document.querySelectorAll('.more-details'),
@@ -15,15 +15,31 @@ function moreDetails() {
     buttonsArr.map(btn =>  {
         btn.addEventListener('click', () => {
             document.getElementById('modal').classList.remove('hide');
+            document.querySelector('.modal-content').classList.remove('hide');
         });
     });
-    
+}
+
+function applyForEvent() {
+    let buttons = document.querySelectorAll('.apply'),
+        buttonsArr = Array.prototype.slice.call(buttons);
+
+        buttonsArr.map(btn =>  {
+            btn.addEventListener('click', () => {
+                document.getElementById('modal').classList.remove('hide');
+                document.querySelector('.success').classList.remove('hide');
+
+                btn.setAttribute('disabled', 'disabled');
+                btn.innerHTML = "Applied";
+            });
+        });
 }
 
 function init() {
     closeModal();
     // openModal();
     moreDetails();
+    applyForEvent();
 }
 
 document.querySelector('body').addEventListener('load', init());
