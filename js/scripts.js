8f28592e14f34520e87849c13a3cf226ef483e28
Karol Harumi <karol.harumi@hotmail.com>
2020-08-30 12:59:58 -0600

    Dynamic modal content

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,65 @@
+const contentModal = [
+    {
+        "id": "meet_brazil",
+        "title": "MeetUp Brazil",
+        "place": "Sao Paulo - Brazil",
+        "date": "October 14th & October 19th 2020"
+    },
+    {
+        "id": "meet_chile",
+        "title": "MeetUp Chile",
+        "place": "Santiago - Chile",
+        "date": "October 14th & October 19th 2020"
+    },
+    {
+        "id": "meet_colombia",
+        "title": "MeetUp Colombia",
+        "place": "Medellin - Colombia",
+        "date": "October 14th & October 19th 2020"
+    },
+    {
+        "id": "meet_argentina",
+        "title": "MeetUp Argentina",
+        "place": "Rosario - Argentina",
+        "date": "October 14th & October 19th 2020"
+    },
+    {
+        "id": "leap_brazil",
+        "title": "Sao Paulo Leap",
+        "place": "Sao Paulo - Brazil",
+        "date": "October 14th & October 19th 2020"
+    },
+    {
+        "id": "mission_colombia",
+        "title": "Medellin Recruiting Mission",
+        "place": "Medellin - Colombia",
+        "date": "October 14th & October 19th 2020"
+    },
+    {
+        "id": "hack_chile",
+        "title": "Santiago VanHackathon",
+        "place": "Santiago - Chile",
+        "date": "October 14th & October 19th 2020"
+    },
+    {
+        "id": "webinar_brazil",
+        "title": "Brazil Webinar",
+        "place": "-",
+        "date": "October 14th & October 19th 2020"
+    },
+    {
+        "id": "webinar_chile",
+        "title": "Chile Webinar",
+        "place": "-",
+        "date": "October 14th & October 19th 2020"
+    }
+]
+
 function closeModal() {
     document.querySelector('.close').addEventListener('click', () => {
         document.getElementById('modal').classList.add('hide');
     });
 }
-
-// function openModal() {
-//     document.getElementById('modal').classList.remove('hide');
-// }
 
 function moreDetails() {
     let buttons = document.querySelectorAll('.more-details'),
@@ -16,6 +69,16 @@ function moreDetails() {
         btn.addEventListener('click', () => {
             document.getElementById('modal').classList.remove('hide');
             document.querySelector('.modal-content').classList.remove('hide');
+
+            contentModal.filter((content) => {    
+                if(content.id === btn.getAttribute('data-event')) {
+                    document.querySelector('.modal-content h2').innerHTML = `${content.title}`;
+                    document.querySelector('.modal-content h3').innerHTML = `${content.date}`;
+                    document.querySelector('.modal-content .place').innerHTML = `${content.place}`;
+                }
+            
+            })
+
         });
     });
 }
@@ -37,7 +100,6 @@ function applyForEvent() {
 
 function init() {
     closeModal();
-    // openModal();
     moreDetails();
     applyForEvent();
 }
